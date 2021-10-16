13b9815115145909d183883ffd90a2023a379165
sakhiofsakshi <imsakshisharma04@gmail.com>
2021-10-16 07:03:37 +0000

    Initial Alioth Bringup

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The AospExtended Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/radiant_alioth.mk
+
+COMMON_LUNCH_CHOICES := \
+    radiant_alioth-user \
+    radiant_alioth-userdebug \
+    radiant_alioth-eng
